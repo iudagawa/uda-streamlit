59442f4ee85e8974f146c32f68b4bdcc71300c08
Udagawa Isao(宇田川 勲) <3610412@phchd.com>
2021-11-08 11:03:38 +0900

    2st

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import  streamlit as st
 import numpy as np
+import time
 
 st.title('Streamlit 入門')
-st.write('プログレスバー')
+st.write('プログレスバーの表示')
 'Start'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'count{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)     # 0.1 sec wait
+
+'Done!!'
